6368ee95ce425713946d0ea9724f996d1973f043
Tien Tran Ba <tien.tran@viedoc.com>
2024-08-27 13:53:41 +0700

    wait element display

--- background.js
+++ background.js
@@ -11,47 +11,93 @@ chrome.runtime.onMessage.addListener((request) => {
 });
 
 function extractInformation(shouldExtractAllComments, commentCount) {
-    // base information
-    const currentUrl = window.location.href;
-    const location = document.querySelector(".DUwDvf.lfPIob")?.innerText
-    const thumbnail = document.querySelector(".RZ66Rb.FgCUCc img")?.src;
-    const totalRating = document.querySelector(".F7nice span span")?.innerText ||  0;
+    // Utility function to wait for an element to appear
+    function waitForElement(selector, hasChild = false, timeout = 10000, interval = 100) {
+        return new Promise((resolve, reject) => {
+            const startTime = Date.now();
+    
+            const checkInterval = setInterval(() => {
+                const element = document.querySelector(selector);
+                
+                // check the condition to determine that element already displayed
+                if (element && (!hasChild || element.hasChildNodes())) {
+                    clearInterval(checkInterval);
+                    resolve(element);
+                } else if (Date.now() - startTime > timeout) {
+                    clearInterval(checkInterval);
+                    reject(`Timeout: Element ${selector} not found within ${timeout}ms`);
+                }
+            }, interval);
+        });
+    }
 
-    const addressElement = document.querySelector('[data-item-id="address"]');
-    const address = addressElement?.querySelector('.Io6YTe')?.innerText || 'Address not found';
+    async function switchToTab(index, waitForSelector) {
+        const tabLists = document.querySelector('div.RWPxGd[role="tablist"]')?.childNodes;
+        tabLists[index].click();
+        await waitForElement(waitForSelector);
+    }
+   
+    async function basicInfomation(){
+           // wait for the tab to be activate
+            if(!document.querySelector('.DUwDvf.lfPIob')){
+                await switchToTab(0, '.DUwDvf.lfPIob'); // Wait for the location element
+            }
+            
+            const currentUrl = window.location.href;
+            const location = document.querySelector(".DUwDvf.lfPIob")?.innerText
+            const thumbnail = document.querySelector(".RZ66Rb.FgCUCc img")?.src;
+            const totalRating = document.querySelector(".F7nice span span")?.innerText ||  0;
 
-    // Select all rows in the table that contains the opening hours information
-    const openingHoursRows = document.querySelectorAll('tr.y0skZc');
+            const address = document.querySelector('[data-item-id="address"]')?.querySelector('.Io6YTe')?.innerText || '';
 
-    let openingHours = [];
+            // Select all rows in the table that contains the opening hours information
+            const openingHoursRows = document.querySelectorAll('tr.y0skZc');
 
-    openingHoursRows.forEach(row => {
-        const day = row.querySelector('td.ylH6lf div')?.innerText || 'Unknown Day';
-        const hours = row.querySelector('td.mxowUb li.G8aQO')?.innerText || 'Unknown Hours';
-        openingHours.push({ day, hours });
-    });
+            let openingHours = [];
 
-    // website
-    const websiteElement = document.querySelector('.rogA2c.ITvuef .fontBodyMedium');
-    const webSite = websiteElement?.innerText || '';
+            openingHoursRows.forEach(row => {
+                const day = row.querySelector('td.ylH6lf div')?.innerText || 'Unknown Day';
+                const hours = row.querySelector('td.mxowUb li.G8aQO')?.innerText || 'Unknown Hours';
+                openingHours.push({ day, hours });
+            });
 
+            const webSite = document.querySelector('.rogA2c.ITvuef .fontBodyMedium')?.innerText || '';
+            const phoneNumber = document.querySelector('.AeaXub .rogA2c .fontBodyMedium')?.innerText || '';
 
-     // Get the inner text of the selected element
-    const phoneNumberElement = document.querySelector('.AeaXub .rogA2c .fontBodyMedium');
-    const phoneNumber = phoneNumberElement?.innerText || '';
+            return {currentUrl, location, thumbnail, totalRating, address, openingHours, webSite, phoneNumber};
+    }
 
+    async  function getComments(){
+        
+        // wait for the tab to be activate
+        if(!document.querySelector('.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde')){
+            await switchToTab(1, '.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde'); // Wait for the reviews container
+        }
 
-    let reviews = [];
-    const reviewElements = document.querySelectorAll('.jJc9Ad');
-    const limit = shouldExtractAllComments ? reviewElements.length : Math.min(reviewElements.length, commentCount);
+        // wait for review list are display
+        await waitForElement('div[class="m6QErb XiKgde "]', true);
 
-    for (let i = 0; i < limit; i++) {
-        const el = reviewElements[i];
-        const user = el.querySelector('.d4r55')?.innerText || 'Unknown User';
-        const rating = el.querySelector('span[class*="kvMYJc"]')?.querySelectorAll('.elGi1d')?.length || 'No Rating';
-        const comment = el.querySelector('.MyEned')?.innerText || 'No Comment';
-        const commentTime  = el.querySelector('.rsqaWe')?.innerText || 'Unknown';
+        let reviews = [];
+        const reviewArea = document.querySelector(".m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde")?.querySelector('div[class="m6QErb XiKgde "]');
+        var reviewNodes = reviewArea.querySelectorAll('.fontBodyMedium');
+        const limit = shouldExtractAllComments ? reviewNodes.length : Math.min(reviewNodes.length, commentCount);
 
+        for (let i = 0; i < limit; i++) {
+            const el = reviewNodes[i];
+            const user = el.querySelector('.d4r55')?.innerText || 'Unknown User';
+            const rating = el.querySelector('span[class*="kvMYJc"]')?.querySelectorAll('.elGi1d')?.length || '0';
+            const comment = el.querySelector('.MyEned')?.innerText || '';
+            const commentTime  = el.querySelector('.rsqaWe')?.innerText || '';
+
+            const images = extractImages(el);
+
+            reviews.push({ user, rating, comment, commentTime, images });
+        }
+
+        return reviews;
+    }
+
+    function extractImages(el){
         // extract images
         const imageElements = el.querySelector('.KtCyie')?.childNodes;
         const images =[];
@@ -65,11 +111,14 @@ function extractInformation(shouldExtractAllComments, commentCount) {
                 }
             }
         }
-        
-        reviews.push({ user, rating, comment, commentTime, images });
     }
 
-    console.log({
-        currentUrl, location, thumbnail, totalRating, address, openingHours, webSite, phoneNumber, reviews
-    });
+    async function main() {
+        const { currentUrl, location, thumbnail, totalRating, address, openingHours, webSite, phoneNumber } = await basicInfomation();
+        const reviews = await getComments();
+
+        console.log({ currentUrl, location, thumbnail, totalRating, address, openingHours, webSite, phoneNumber, reviews });
+    }
+
+    main();
 }
